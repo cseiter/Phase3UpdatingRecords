b719be1174b870a48961191a49b0916960c22545
chris <cseiter@gmail.com>
2021-07-24 16:40:15 -0400

    save pass first half

--- lib/student.rb
+++ lib/student.rb
@@ -33,4 +33,13 @@ class Student
     DB[:conn].execute(sql)
   end
 
+  def save
+    sql = <<-SQL
+    insert into students(name, grade)
+    values(?, ?)
+    SQL
+    DB[:conn].execute(sql,self.name,self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+  end
+
 end
